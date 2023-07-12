463c7700f5fd87483335585bad27c7bec359ba77
koginomama <konomamadeha@gmail.com>
2023-07-12 18:42:13 +0900

    基本部分の完成

--- app/javascript/weather.js
+++ app/javascript/weather.js
@@ -9,48 +9,178 @@ document.getElementById('refresh-button').addEventListener('click', function() {
   .then(data => {
     const geolat = data.lat
     const geolon = data.lon
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolat}&lon=${geolon}&cnt=1&lang=ja&appid=${apikey}&units=metric`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolat}&lon=${geolon}&cnt=9&lang=ja&appid=${apikey}&units=metric`;
 
   fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    const temperature = Math.round(data.list[0].main.temp); // 温度情報の取得
-    const temp_max = Math.round(data.list[0].main.temp_max);
-    const temp_min = Math.round(data.list[0].main.temp_min);
-    const weatherPattern = data.list[0].weather[0].icon; 
-    const temperatureElement = document.getElementById('weather-temperature'); // 温度を表示する要素の取得
+    const temppop3 = data.list[0].pop; // 降水確率情報の取得、変数の数字は時間帯を示している。
+    const temppop9 = data.list[2].pop; 
+    const temppop15 = data.list[4].pop; 
+    const temppop21 = data.list[6].pop; 
+    const temp_max = (data.list[0].main.temp_max); // 最高気温
+    const temp_max9 = Math.round(data.list[2].main.temp_max);
+    const temp_max15 = Math.round(data.list[4].main.temp_max);
+    const temp_max21 = Math.round(data.list[6].main.temp_max);
+    const temp_min = Math.round(data.list[0].main.temp_min); // 最低気温
+    const temp_min9 = Math.round(data.list[2].main.temp_min); 
+    const temp_min15 = Math.round(data.list[4].main.temp_min); 
+    const temp_min21 = Math.round(data.list[6].main.temp_min); 
+    const weatherPattern = data.list[0].weather[0].icon;  // 天候アイコン情報
+    const weatherPattern9 = data.list[2].weather[0].icon;
+    const weatherPattern15 = data.list[4].weather[0].icon;
+    const weatherPattern21 = data.list[6].weather[0].icon;
+    const tempAverage = Math.round(data.list[2].main.temp);
+    const temperatureElement3 = document.getElementById('weather-temperature');// 温度を表示する要素の取得
+    const temperatureElement9 = document.getElementById('weather-temperature9'); 
+    const temperatureElement15 = document.getElementById('weather-temperature15');
+    const temperatureElement21 = document.getElementById('weather-temperature21');
     const tempMaxElement = document.getElementById('temp-max');
+    const tempMaxElement9 = document.getElementById('temp-max9');
+    const tempMaxElement15 = document.getElementById('temp-max15');
+    const tempMaxElement21 = document.getElementById('temp-max21');
     const tempMinElement = document.getElementById('temp-min');
+    const tempMinElement9 = document.getElementById('temp-min9');
+    const tempMinElement15 = document.getElementById('temp-min15');
+    const tempMinElement21 = document.getElementById('temp-min21');
     const weatherIconElement = document.getElementById('weather-icon');
-    if (weatherPattern === '01d') {
-      weatherIconElement.innerHTML = '<img src="/assets/hare.png" alt="晴れ">';
-    } else if (weatherPattern === '02d') {
-      weatherIconElement.innerHTML = '<img src="/assets/kumori_hare.png" alt="曇り晴れ">';
-    } else if (weatherPattern === '03d') {
-      weatherIconElement.innerHTML = '<img src="/assets/kumori.png" alt="曇り">';
-    } else if (weatherPattern === '04d') {
-      weatherIconElement.innerHTML = '<img src="/assets/kumori.png" alt="曇り">';
-    } else if (weatherPattern === '09d') {
-      weatherIconElement.innerHTML = '<img src="/assets/niwaka_ame.png" alt="にわか雨">';
-    } else if (weatherPattern === '10d') {
-      weatherIconElement.innerHTML = '<img src="/assets/rainy.png" alt="雨">';
-    } else if (weatherPattern === '11d') {
-      weatherIconElement.innerHTML = '<img src="/assets/kaminari.png" alt="雷雨">';
-    } else if (weatherPattern === '13d') {
-      weatherIconElement.innerHTML = '<img src="/assets/yuki.png" alt="雪">';
-    } else if (weatherPattern === '50d') {
-      weatherIconElement.innerHTML = '<img src="/assets/yama_kiri.png" alt="霧">';
+    const weatherIconElement9 = document.getElementById('weather-icon9');
+    const weatherIconElement15 = document.getElementById('weather-icon15');
+    const weatherIconElement21 = document.getElementById('weather-icon21');
+    
+
+
+    function setWeatherIcon(iconName, element) {
+      let iconSrc = '';
+      switch (iconName) {
+        case '01d':
+        case '01n':
+          iconSrc = '/assets/hare.png';
+          break;
+        case '02d':
+        case '02n':
+          iconSrc = '/assets/kumori_hare.png';
+          break;
+        case '03d':
+        case '03n':
+        case '04d':
+        case '04n':
+          iconSrc = '/assets/kumori.png';
+          break;
+        case '09d':
+        case '09n':
+          iconSrc = '/assets/niwaka_ame.png';
+          break;
+        case '10d':
+        case '10n':
+          iconSrc = '/assets/rainy.png';
+          break;
+        case '11d':
+        case '11n':
+          iconSrc = '/assets/kaminari.png';
+          break;
+        case '13d':
+        case '13n':
+          iconSrc = '/assets/yuki.png';
+          break;
+        case '50d':
+        case '50n':
+          iconSrc = '/assets/yama_kiri.png';
+          break;
+        default:
+          iconSrc = '';
+      }
+      element.innerHTML = `<img src="${iconSrc}" alt="${iconName}">`;
     }
 
-    temperatureElement.textContent = temperature + "°"; // 温度を要素に表示
-    tempMaxElement.textContent = temp_max + '°';
-    tempMinElement.textContent = temp_min + '°';
+    temperatureElement3.textContent = Math.round(temppop3 * 100) + "%"; // 温度を要素に表示
+    temperatureElement9.textContent = Math.round(temppop9 * 100) + "%";
+    temperatureElement15.textContent = Math.round(temppop15 * 100) + "%";
+    temperatureElement21.textContent = Math.round(temppop21 * 100) + "%";
+    tempMaxElement.textContent = Math.round(temp_max) ;
+    tempMaxElement9.textContent = temp_max9 ;
+    tempMaxElement15.textContent = temp_max15 ;
+    tempMaxElement21.textContent = temp_max21 ;
+    tempMinElement.textContent = temp_min ;
+    tempMinElement9.textContent = temp_min9 ;
+    tempMinElement15.textContent = temp_min15 ;
+    tempMinElement21.textContent = temp_min21 ;
+    setWeatherIcon(weatherPattern, weatherIconElement);
+    setWeatherIcon(weatherPattern9, weatherIconElement9);
+    setWeatherIcon(weatherPattern15, weatherIconElement15);
+    setWeatherIcon(weatherPattern21, weatherIconElement21);
 
-    // if (temperature >= 25) {
-    //   document.getElementById('weather-icon').innerHTML = '<img src="/assets/hare.png" alt="晴れ">';
-    // } else {
-    //   document.getElementById('weather-icon').innerHTML = '<img src="/assets/rainy.png" alt="雨">';
-    // }
+    // 温度による条件分岐を行い、適切なテキストと画像を表示
+    const imageElement = document.getElementById('select-image');
+    const textElement = document.getElementById('select-text');
+    const textExpElement = document.getElementById('select-text-exp');
+
+
+    if (tempAverage < 0) {
+      textElement.textContent = "肌着2枚＋長袖ウエア" ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test0b.jpg';
+      } else {
+        imageElement.src = '/assets/test0g.jpg';
+      }
+    } else if (tempAverage >= 0 && tempAverage <= 5) {
+      textElement.textContent = "肌着2枚＋長袖ウエア " ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test5b.jpg';
+      } else {
+        imageElement.src = '/assets/test5g.jpg';
+      };
+    } else if (tempAverage >= 6 && tempAverage <= 10) {
+      textElement.textContent = "肌着1〜2枚＋長袖ウエア" ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test10b.jpg';
+      } else {
+        imageElement.src = '/assets/test10g.jpg';
+      };
+    } else if (tempAverage >= 11 && tempAverage <= 15) {
+      textElement.textContent = "肌着1〜2枚＋長袖ウエア" ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test15b.jpg';
+      } else {
+        imageElement.src = '/assets/test15g.jpg';
+      };
+    } else if (tempAverage >= 16 && tempAverage <= 20) {
+      textElement.textContent = "肌着1枚＋半袖シャツ1枚" ;
+      textExpElement.textContent = "気温が上がって汗ばむ日にはウエアを半袖に。室内では肌着だけの重ね着も◎。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test20b.jpg';
+      } else {
+        imageElement.src = '/assets/test20g.jpg';
+      };
+    } else if (tempAverage >= 21 && tempAverage <= 25) {
+      textElement.textContent = "肌着1枚＋半袖シャツ1枚" ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test20b.jpg';
+      } else {
+        imageElement.src = '/assets/test20g.jpg';
+      };
+    } else if (tempAverage >= 26 && tempAverage <= 30) {
+      textElement.textContent = "肌着1枚＋半袖シャツ1枚" ;
+      textExpElement.textContent = "肌寒い日には肌着を1枚プラスして体温調節。冷え込む朝はおくるみを用意しましょう。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test26b.jpg';
+      } else {
+        imageElement.src = '/assets/test26g.jpg';
+      };
+    } else {
+      textElement.textContent = "肌着1枚or半袖シャツ1枚" ;
+      textExpElement.textContent = "室温も高くなる猛暑日は肌着1枚で過ごせます。エアコンの効かせすぎに注意し、冷房対策として薄手の長袖があると安心。汗をたくさんかくようなら、汗取りパットを背中に入れてあせも対策を。" ;
+      if (currentUserGender == 1) {
+        imageElement.src = '/assets/test30b.jpg';
+      } else {
+        imageElement.src = '/assets/test30g.jpg';
+      };
+    }
   })
   })
   .catch(error => {
